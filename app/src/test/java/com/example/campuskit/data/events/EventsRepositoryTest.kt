ad1b1e908385947dd234faa1a77d0eaa7fe8731e
Sandesh <sandeshraj0410@gmail.com>
2026-03-26 15:58:06 +0530

    test: update EventsRepositoryTest for Room-backed repository

--- app/src/test/java/com/example/campuskit/data/events/EventsRepositoryTest.kt
+++ app/src/test/java/com/example/campuskit/data/events/EventsRepositoryTest.kt
@@ -1,18 +1,57 @@
 package com.example.campuskit.data.events
 
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class EventsRepositoryTest {
 
+    private val mockDao: EventDao = mockk(relaxed = true)
+
     @Test
-    fun getEvents_ReturnsMockEventsFlow() = runTest {
-        val repository = EventsRepository()
-        
+    fun getEvents_mapsEntitiesToDomainModels() = runTest {
+        val entities = listOf(
+            EventEntity("1", "Hackathon", "Feb 15", "Lab 1", "", "Coding Club", "Desc")
+        )
+        coEvery { mockDao.getAllEvents() } returns flowOf(entities)
+
+        val repository = EventsRepository(mockDao)
         val events = repository.getEvents().first()
-        
-        assertEquals(MockEvents.getEvents(), events)
+
+        assertEquals(1, events.size)
+        assertEquals("Hackathon", events[0].title)
+        assertEquals("Coding Club", events[0].organizer)
+    }
+
+    @Test
+    fun seedIfEmpty_insertsWhenCountIsZero() = runTest {
+        coEvery { mockDao.getCount() } returns 0
+
+        val repository = EventsRepository(mockDao)
+        repository.seedIfEmpty()
+
+        io.mockk.coVerify { mockDao.insertAll(any()) }
+    }
+
+    @Test
+    fun seedIfEmpty_skipsWhenDataExists() = runTest {
+        coEvery { mockDao.getCount() } returns 5
+
+        val repository = EventsRepository(mockDao)
+        repository.seedIfEmpty()
+
+        io.mockk.coVerify(exactly = 0) { mockDao.insertAll(any()) }
+    }
+
+    @Test
+    fun toggleReminder_delegatesToDao() = runTest {
+        val repository = EventsRepository(mockDao)
+        repository.toggleReminder("event_1")
+
+        io.mockk.coVerify { mockDao.toggleReminder("event_1") }
     }
 }
